cf8eeef9f57e434ab5eeb7a6fc57cc4f136980d2
LeoLe101 <p.le6796@edmail.edcc.edu>
2017-10-19 15:40:10 -0700

    Initial Commit

---
+++ CSS-342-Assignment-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSS-342-Assignment-2
+//
+//  Created by Leo Le on 10/19/17.
+//  Copyright © 2017 Leo Le. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
